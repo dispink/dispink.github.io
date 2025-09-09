2803fbcb2e97f2f5e973360f254136da6b6d3f93
dispink-mini <dispink@icloud.com>
2025-09-09 04:54:31 +0000

    Solve dependency issues

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,2 +1,6 @@
-pip3 install --upgrade --user pip; 
+rm Gemfile.lock || true;
+sudo apt update && sudo apt upgrade -y && sudo apt install ruby-dev ruby-bundler nodejs -y;
+bundle install;
+bundle clean --force;
+pip3 install --upgrade --user pip;
 pip3 --no-cache-dir install --user -r .devcontainer/requirements.txt;
